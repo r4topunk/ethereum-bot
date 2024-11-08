6980d9528bc6583fbca69abbebff4fae23db1dae
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-11-07 23:55:42 -0300

    feat: add logging functionality and update dependencies

--- constants.js
+++ constants.js
@@ -8,3 +8,4 @@ export const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 export const CONTRACT_ADDRESS = '0x997020E5F59cCB79C74D527Be492Cc610CB9fA2B';
 export const MIN_ETH_VALUE = parseEther('0.1');
 export const BUY_VALUE = parseEther('0.000333');
+export const LOG_FILE_PATH = "log.txt";
--- utils.js
+++ utils.js
@@ -1,6 +1,10 @@
 import chalk from 'chalk';
+import { LOG_FILE_PATH } from './constants.js';
+import { appendFileSync } from 'fs';
 
 export function logWithTimestamp(message, color = chalk.white) {
   const now = new Date().toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" });
-  console.log(color(`[${now}] ${message}`));
+  const logMessage = `[${now}] ${message}`;
+  appendFileSync(LOG_FILE_PATH, logMessage + '\n');
+  console.log(color(logMessage));
 }
