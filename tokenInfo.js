35713dce78111cd2111c149fe29b2e075875bcd9
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-11-10 07:42:24 -0300

    fix: token balance format

--- tokenInfo.js
+++ tokenInfo.js
@@ -1,4 +1,3 @@
-
 import { logWithTimestamp, getEthZeros } from "./utils.js";
 import { provider } from "./provider.js";
 import { wallet } from "./wallet.js";
@@ -7,119 +6,148 @@ import { formatEther, formatUnits, id, Interface, toBigInt } from "ethers";
 import { UNISWAP_V3_POOL_ABI } from "./uniswap-v3-abi.js";
 import terminalLink from "terminal-link";
 
-export async function getTokenWorthInEth(contract, totalSpentMap) {
+export async function getTokenWorthInEth(tokenContract, totalSpentMap) {
   try {
-    const tokenName = await contract.name();
-    const contractAddress = await contract.getAddress();
+    const tokenName = await tokenContract.name();
+    const tokenAddress = await tokenContract.getAddress();
 
-    const baseLink = terminalLink(
-      "Base",
-      `https://basescan.org/address/${contractAddress}`
-    );
-    const wowLink = terminalLink("Wow", `https://wow.xyz/${contractAddress}`);
-    const dexScreen = terminalLink(
-      "DexScreen",
-      `https://dexscreener.com/base/${contractAddress}`
-    );
-
-    const links = terminalLink.isSupported
-      ? `${tokenName} => ${baseLink} | ${wowLink} | ${dexScreen}`
-      : `${tokenName}\n` + chalk.blue(`${baseLink}\n${wowLink}\n${dexScreen}`);
-
+    const links = generateTokenLinks(tokenName, tokenAddress);
     logWithTimestamp(links, chalk.magenta, false);
-    logWithTimestamp(`Address ${contractAddress}`, chalk.black, false);
+    logWithTimestamp(`Address ${tokenAddress}`, chalk.black, false);
 
-    const balance = await contract.balanceOf(wallet.address);
-    logWithTimestamp(`Balance ${Number(formatUnits(balance, 18)).toFixed("8")}`, chalk.black, false);
+    const tokenBalance = await getTokenBalance(tokenContract);
+    logWithTimestamp(`Balance ${formatTokenBalance(tokenBalance)}`, chalk.black, false);
 
-    // 0 = BONDING_CURVE, 1 = UNISWAP_POOL
-    const marketType = await contract.marketType();
-    logWithTimestamp(
-      `Type    ${marketType === 0n ? "BONDING_CURVE" : "UNISWAP_POOL"}`,
-      chalk.black,
-      false
-    );
+    const marketType = await getMarketType(tokenContract);
+    logWithTimestamp(`Type    ${marketType}`, chalk.black, false);
 
-    const filter = {
-      fromBlock: 22119142,
-      toBlock: "latest",
-      address: contractAddress,
-      topics: [
-        id("Transfer(address,address,uint256)"),
-        null,
-        null,
-      ],
-    };
-
-    const logs = await provider.getLogs(filter);
-    const lastTransactionLog = logs[logs.length - 1];
-    const lastTransaction = await provider.getTransaction(lastTransactionLog.transactionHash);
-    const block = await provider.getBlock(lastTransaction.blockNumber);
-    const lastTransactionDate = new Date(block.timestamp * 1000).toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" });
-
-    if (marketType === 0n) {
-      // Decode the transaction data to get the function name
-      const iface = new Interface(contract.interface.fragments);
-      const parsedTransaction = iface.parseTransaction({ data: lastTransaction.data });
-      // console.log(formatUnits(parsedTransaction.args[0], 18));
-      const functionName = parsedTransaction.name;
-      logWithTimestamp(`Lastx   [${functionName.toUpperCase()}] ${lastTransactionDate}`, chalk.black, false);
+    const lastTransactionDate = await getLastTransactionDate(tokenAddress);
+    if (marketType === "BONDING_CURVE") {
+      const lastFunctionName = await getLastTransactionFunctionName(tokenContract, tokenAddress);
+      logWithTimestamp(`Lastx   [${lastFunctionName}] ${lastTransactionDate}`, chalk.black, false);
     }
 
-    const totalSpentWei = totalSpentMap[contractAddress] || toBigInt(0);
-    let spentNumOfZeros = getEthZeros(formatEther(totalSpentWei));
-    logWithTimestamp(
-      `Spent   [0x${spentNumOfZeros}] ${Number(formatEther(totalSpentWei)).toFixed(8)} ETH`,
-      chalk.black,
-      false
-    );
+    const totalSpentWei = totalSpentMap[tokenAddress] || toBigInt(0);
+    const formattedSpent = formatEthValue(totalSpentWei);
+    logWithTimestamp(`Spent   ${formattedSpent}`, chalk.black, false);
 
-    if (balance > 0) {
-      const poolAddress = await contract.poolAddress();
-      let ethWorthInWei;
+    if (tokenBalance > 0) {
+      const tokenWorthInWei = await calculateTokenWorth(tokenContract, tokenBalance, marketType);
+      const formattedWorth = formatEthValue(tokenWorthInWei);
+      logWithTimestamp(`Worth   ${formattedWorth}`, chalk.black, false);
 
-      if (marketType === 0n) {
-        ethWorthInWei = await contract.getTokenSellQuote(balance);
-      } else {
-        const tokenPriceInWei = await getTokenPriceUniswap(poolAddress);
-        ethWorthInWei = tokenPriceInWei * balance / BigInt(10 ** 18);
-      }
-
-      const ethWorth = formatEther(ethWorthInWei);
-      const balanceNumOfZeros = getEthZeros(ethWorth);
-
+      const percentageDifference = calculateDifferencePercentage(tokenWorthInWei, totalSpentWei);
       logWithTimestamp(
-        `Worth   [0x${balanceNumOfZeros}] ${Number(ethWorth).toFixed(8)} ETH`,
-        chalk.black,
+        `Diff    ${percentageDifference}%`,
+        percentageDifference > 0 ? chalk.green : chalk.red,
         false
       );
 
-      const differenceInWei = ethWorthInWei - totalSpentWei;
-      const percentageDifference = (differenceInWei * 100n) / totalSpentWei;
-      const percentage = Number(percentageDifference);
-      logWithTimestamp(
-        `Diff    ${percentage}%`,
-        percentage > 0 ? chalk.green : chalk.red,
-        false
-      );
-
-      return { ethWorth, percentage, balance };
+      return { ethWorth: formatEther(tokenWorthInWei), percentage: percentageDifference, balance: tokenBalance };
     } else {
       logWithTimestamp(`No tokens to calculate worth`, chalk.yellow, false);
       return 0;
     }
   } catch (error) {
-    logWithTimestamp(
-      `Error calculating token worth: ${error}`,
-      chalk.red,
-      false
-    );
+    logWithTimestamp(`Error calculating token worth: ${error}`, chalk.red, false);
     return 0;
   }
 }
 
-export async function getTokenPrice(contract) {
-  const ethWorthInWei = await contract.getTokenSellQuote(balance);
+// Helper functions
+
+function generateTokenLinks(tokenName, tokenAddress) {
+  const baseLink = terminalLink(
+    "Base",
+    `https://basescan.org/address/${tokenAddress}`
+  );
+  const wowLink = terminalLink("Wow", `https://wow.xyz/${tokenAddress}`);
+  const dexScreen = terminalLink(
+    "DexScreen",
+    `https://dexscreener.com/base/${tokenAddress}`
+  );
+
+  return terminalLink.isSupported
+    ? `${tokenName} => ${baseLink} | ${wowLink} | ${dexScreen}`
+    : `${tokenName}\n` + chalk.blue(`${baseLink}\n${wowLink}\n${dexScreen}`);
+}
+
+async function getTokenBalance(tokenContract) {
+  return await tokenContract.balanceOf(wallet.address);
+}
+
+function formatTokenBalance(balance) {
+  return Number(formatUnits(balance, 18)).toFixed(8);
+}
+
+async function getMarketType(tokenContract) {
+  const marketTypeValue = await tokenContract.marketType();
+  return marketTypeValue === 0n ? "BONDING_CURVE" : "UNISWAP_POOL";
+}
+
+async function getLastTransactionDate(tokenAddress) {
+  const filter = {
+    fromBlock: 22119142,
+    toBlock: "latest",
+    address: tokenAddress,
+    topics: [
+      id("Transfer(address,address,uint256)"),
+      null,
+      null,
+    ],
+  };
+
+  const logs = await provider.getLogs(filter);
+  const lastTransactionLog = logs[logs.length - 1];
+  const lastTransaction = await provider.getTransaction(lastTransactionLog.transactionHash);
+  const block = await provider.getBlock(lastTransaction.blockNumber);
+  return new Date(block.timestamp * 1000).toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" });
+}
+
+async function getLastTransactionFunctionName(tokenContract, tokenAddress) {
+  const filter = {
+    fromBlock: 22119142,
+    toBlock: "latest",
+    address: tokenAddress,
+    topics: [
+      id("Transfer(address,address,uint256)"),
+      null,
+      null,
+    ],
+  };
+
+  const logs = await provider.getLogs(filter);
+  const lastTransactionLog = logs[logs.length - 1];
+  const lastTransaction = await provider.getTransaction(lastTransactionLog.transactionHash);
+
+  const iface = new Interface(tokenContract.interface.fragments);
+  const parsedTransaction = iface.parseTransaction({ data: lastTransaction.data });
+  return parsedTransaction.name.toUpperCase();
+}
+
+function formatEthValue(valueInWei) {
+  const zeros = getEthZeros(formatEther(valueInWei));
+  return `[0x${zeros}] ${Number(formatEther(valueInWei)).toFixed(8)} ETH`;
+}
+
+async function calculateTokenWorth(tokenContract, tokenBalance, marketType) {
+  if (marketType === "BONDING_CURVE") {
+    return await tokenContract.getTokenSellQuote(tokenBalance);
+  } else {
+    const poolAddress = await tokenContract.poolAddress();
+    const tokenPriceInWei = await getTokenPriceUniswap(poolAddress);
+    return (tokenPriceInWei * toBigInt(tokenBalance)) / BigInt(1e18);
+  }
+}
+
+function calculateDifferencePercentage(tokenWorthInWei, totalSpentWei) {
+  const differenceInWei = tokenWorthInWei - totalSpentWei;
+  const percentageDifference = (differenceInWei * 100n) / totalSpentWei;
+  return Number(percentageDifference);
+}
+
+export async function getTokenPrice(tokenContract) {
+  const ethWorthInWei = await tokenContract.getTokenSellQuote(balance);
   const ethWorth = formatEther(ethWorthInWei);
   return ethWorth
 }
